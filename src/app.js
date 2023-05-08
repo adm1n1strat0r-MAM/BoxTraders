66e99c1baa1c87879eb9e2ce33276d78d7f96835
adm1n1strat0r-MAM <masadmunawar915@gmail.com>
2023-05-08 11:07:33 -0700

    run the app and connect with the database server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-var express = require('express');
+const express = require("express");
+const cors = require("cors");
+const config = require("./config/config");
+const PORT = config.PORT;
+require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port no ${PORT}`);
+});
